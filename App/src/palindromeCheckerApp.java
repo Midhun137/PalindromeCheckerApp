9fa2e3e4415457d0e964cfec6dd7ecf5384ddf35
RA2411003012595 <midhuna1307@gmail.com>
2026-03-05 23:09:13 +0530

    UC8

--- App/src/palindromeCheckerApp.java
+++ App/src/palindromeCheckerApp.java
@@ -1,52 +1,102 @@
-import java.util.Stack;
-import java.util.Queue;
 import java.util.Deque;
 import java.util.LinkedList;
+class PalindromeLinkedList {
 
-class PalindromeQueueStack {
     class PalindromeDeque {
+        // Node class
+        static class Node {
+            char data;
+            Node next;
 
-        public static void main(String[] args) {
-
-            String word = "madam";
-            String word = "racecar";
-
-            Stack<Character> stack = new Stack<>();
-            Queue<Character> queue = new LinkedList<>();
-            Deque<Character> deque = new LinkedList<>();
-
-            // Insert characters into Stack and Queue
-            // Insert characters into deque
-            for (int i = 0; i < word.length(); i++) {
-                char ch = word.charAt(i);
-                stack.push(ch);      // LIFO
-                queue.add(ch);       // FIFO
-                deque.addLast(word.charAt(i));
+            public static void main(String[] args) {
+                Node(char data) {
+                    this.data = data;
+                    this.next = null;
+                }
             }
 
-            boolean isPalindrome = true;
+            String word = "racecar";
+            // Reverse linked list
+            static Node reverse(Node head) {
+                Node prev = null;
+                Node current = head;
 
-            // Compare dequeue (queue) and pop (stack)
-            for (int i = 0; i < word.length(); i++) {
-                // Compare first and last characters
-                while (deque.size() > 1) {
-
-                    char fromQueue = queue.remove(); // FIFO
-                    char fromStack = stack.pop();    // LIFO
-                    char first = deque.removeFirst();
-                    char last = deque.removeLast();
-
-                    if (fromQueue != fromStack) {
-                        if (first != last) {
-                            isPalindrome = false;
-                            break;
-                        }
-                    }
-
-                    // Display result
-                    if (isPalindrome) {
-                        System.out.println(word + " is a Palindrome");
-                    } else {
-                        System.out.println(word + " is NOT a Palindrome");
-                    }
+                Deque<Character> deque = new LinkedList<>();
+                while (current != null) {
+                    Node next = current.next;
+                    current.next = prev;
+                    prev = current;
+                    current = next;
                 }
+
+                // Insert characters into deque
+                for (int i = 0; i < word.length(); i++) {
+                    deque.addLast(word.charAt(i));
+                    return prev;
+                }
+
+                // Check palindrome
+                static boolean isPalindrome(Node head) {
+
+                    Node slow = head;
+                    Node fast = head;
+
+                    // Find middle using fast and slow pointer
+                    while (fast != null && fast.next != null) {
+                        slow = slow.next;
+                        fast = fast.next.next;
+                    }
+
+                    boolean isPalindrome = true;
+                    // Reverse second half
+                    Node secondHalf = reverse(slow);
+                    Node firstHalf = head;
+
+                    // Compare first and last characters
+                    while (deque.size() > 1) {
+                        // Compare halves
+                        while (secondHalf != null) {
+                            if (firstHalf.data != secondHalf.data) {
+                                return false;
+                            }
+                            firstHalf = firstHalf.next;
+                            secondHalf = secondHalf.next;
+                        }
+
+                        char first = deque.removeFirst();
+                        char last = deque.removeLast();
+                        return true;
+                    }
+
+                    public static void main(String[] args) {
+
+                        String word = "madam";
+
+                        Node head = null;
+                        Node tail = null;
+
+                        // Convert string to linked list
+                        for (int i = 0; i < word.length(); i++) {
+                            Node newNode = new Node(word.charAt(i));
+
+                            if (first != last) {
+                                isPalindrome = false;
+                                break;
+                                if (head == null) {
+                                    head = newNode;
+                                    tail = newNode;
+                                } else {
+                                    tail.next = newNode;
+                                    tail = newNode;
+                                }
+                            }
+
+                            // Display result
+                            if (isPalindrome) {
+                                // Print result
+                                if (isPalindrome(head)) {
+                                    System.out.println(word + " is a Palindrome");
+                                } else {
+                                    System.out.println(word + " is NOT a Palindrome");
+                                }
+                            }
